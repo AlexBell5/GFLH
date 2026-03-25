b560ce78547a2e86904e7753ce58a2e656e89a63
Alex Bell <alex.bell@uhsw.com>
2026-03-25 12:01:35 +0000

    changes

--- pages/signup.php
+++ pages/signup.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Sign Up - GFLH</title>
-  <link rel="stylesheet" href="../styles/style.css" />
+  <link rel="stylesheet" href="../styles/sign-up.css" />
   <link rel="stylesheet" href="../styles/navbar.css" />
 </head>
 <body>
